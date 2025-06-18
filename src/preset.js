368406ad7a7797f332636ebc9331b577c696443f
Linzp <sunandmoon001@163.com>
2025-06-18 16:50:59 +0800

    修改配置

--- src/preset.js
+++ src/preset.js
@@ -13,15 +13,17 @@ window.PUBLIC_URL = window.runtimePublicUrl || process.env.PUBLIC_URL;
 
 const baseApiUrl = window.runtimeApiUrl || '';
 window.runtimeGatewayUrl = window.runtimeGatewayUrl || baseApiUrl || 'https://api.gw.leapin-ai.com';
+const appName = 'ai-agent';
+const env = window.runtimeEnv?.['env'] || 'local';
 
 export const globalInit = async () => {
   const ajax = createAjax({
-    baseURL: baseApiUrl,
+    baseURL: env === 'local' ? '' : baseApiUrl,
     errorHandler: error => message.error(error),
     getDefaultHeaders: () => {
       return {
-        env: 'prod',
-        appName: 'ai-agent',
+        env,
+        appName,
         Authorization: `Bearer ${cookie.get('token')}`
       };
     },
